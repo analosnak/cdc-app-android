df9bad3c38511b0de49601ca1ac040eda1245f5e
Ana Losnak <ana.losnak@caelum.com.br>
2020-02-11 20:13:23 -0300

    Criando activity vazia pra ver rodando

---
+++ app/src/main/java/br/com/caelum/casadocodigo/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package br.com.caelum.casadocodigo.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.caelum.casadocodigo.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
